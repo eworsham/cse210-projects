dc27d68638b256713ee2946fc4678d3bfcdf6ba7
eworsham <eworsham64@gmail.com>
2024-04-22 18:02:27 -0400

    Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Get the user's grade percentage
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentageAsText = Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercentageAsText);
+
+        // Output the user's letter grade
+        char letterGrade;
+        if (gradePercentage >= 90) 
+        {
+            letterGrade = 'A';
+        }
+        else if (gradePercentage >= 80) 
+        {
+            letterGrade = 'B';
+        }
+        else if (gradePercentage >= 70) 
+        {
+            letterGrade = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = 'D';
+        }
+        else 
+        {
+            letterGrade = 'F';
+        }
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        // Output if the user's passed or failed the course
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Great Job!! You passed the course.");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
