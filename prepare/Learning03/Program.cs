bfaff489f54c023e7ae1e5cdd440ae0c67417d3c
eworsham <eworsham64@gmail.com>
2024-05-08 15:09:56 -0400

    Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(6);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction();
+        fraction4.SetTop(9);
+        fraction4.SetBottom(8);
+        Console.WriteLine(fraction4.GetTop());
+        Console.WriteLine(fraction4.GetBottom());
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
