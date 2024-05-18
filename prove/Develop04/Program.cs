cde2ba3400328f07b176cca2c58f7ddb70db6f02
eworsham <eworsham64@gmail.com>
2024-05-18 09:02:35 -0400

    Setup Main

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        // Create BreathingActivity object
+        BreathingActivity breathingActivity = new BreathingActivity();
+
+        // Create ReflectingActivity object
+        ReflectingActivity reflectingActivity = new ReflectingActivity();
+
+        // Create ListingActivity object;
+        ListingActivity listingActivity = new ListingActivity();
+        
+        // Menu loop
+        string userInput = "";
+        while (userInput != "4") {
+            // Display menu options and get user's selection
+            DisplayMenu();
+            userInput = Console.ReadLine();
+
+            if (userInput == "1") // Start breathing activity
+            {
+                breathingActivity.Run();
+            }
+            else if (userInput == "2") // Start reflecting activity
+            {
+                reflectingActivity.Run();
+            }
+            else if (userInput == "3") // Start listing activity
+            {
+                listingActivity.Run();
+            }
+
+        }
+    }
+
+    public static void DisplayMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("  1. Start breathing activity");
+        Console.WriteLine("  2. Start reflecting activity");
+        Console.WriteLine("  3. Start listing activity");
+        Console.WriteLine("  4. Quit");
+        Console.WriteLine("Select a choice from the menu");
     }
 }
