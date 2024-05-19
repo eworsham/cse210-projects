d670fc35a080ebce95077042b87af6edd927a394
eworsham <eworsham64@gmail.com>
2024-05-18 20:06:38 -0400

    Finished listing activity

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -22,19 +22,44 @@ public class ListingActivity : Activity
         // Show starting message
         DisplayStartingMessage();
 
+        // Display random prompt
+        Console.WriteLine("List as many responses as you can to the following prompt:");
+        Console.WriteLine($" --- {GetRandomPrompt()} ---");
+        Console.Write("You may being in  ");
+        ShowCountDown(10);
 
+        // Get list from user and display number of items from the list
+        List<string> userList = GetListFromUser();
+        _count = userList.Count;
+        Console.WriteLine($"You listed {_count} items!");
+        Console.WriteLine();
 
         // Show ending message
         DisplayEndingMessage();
     }
 
-    public string GetRandonPrompt()
+    public string GetRandomPrompt()
     {
-        return "";
+        Random random = new Random();
+        int randomIndex = random.Next(_prompts.Count);
+
+        return _prompts[randomIndex];
     }
 
     public List<string> GetListFromUser()
     {
-        return [];
+        List<string> userList = [];
+
+        // Loop until the duration is reached
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            userList.Add(Console.ReadLine());
+        }
+
+        return userList;
     }   
 }
