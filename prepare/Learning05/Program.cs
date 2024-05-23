45348c3baf80e1efe3eb1137501dce75f8ddda5d
eworsham <eworsham64@gmail.com>
2024-05-23 19:31:01 -0400

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Blue", 2);
+        Rectangle rectangle = new Rectangle("Green", 2, 3);
+        Circle circle = new Circle("Yellow", 3);
+
+        List<Shape> shapes = [square, rectangle, circle];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
